a06e40600e2c76a29fd90767d8e0fbb9aeb4914c
talha f basit <talhafbasit@gmail.com>
2020-07-16 22:31:51 -0500

    Added coupled property totalBill and created section and header for that property

--- WeSplit13/ContentView.swift
+++ WeSplit13/ContentView.swift
@@ -9,62 +9,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
         
-        @State private var checkAmount = ""
-        @State private var numberOfPeople = 2
-        @State private var tipPercentage = 2
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
         
-        let tipPercentages = [10,15,20,25,0]
+        return amountPerPerson
         
-        var totalPerPerson: Double {
-            let peopleCount = Double(numberOfPeople + 2)
-            let tipSelection = Double(tipPercentages[tipPercentage])
-            let orderAmount = Double(checkAmount) ?? 0
-            
-            let tipValue = orderAmount / 100 * tipSelection
-            let grandTotal = orderAmount + tipValue
-            let amountPerPerson = grandTotal / peopleCount
-            
-            return amountPerPerson
-            
-        }
+    }
+    
+    var totalBill: Double {
         
+        return totalPerPerson * Double(numberOfPeople + 2)
         
-        var body: some View {
-            NavigationView {
-                Form {
-                    Section {
-                        TextField("Amount", text: $checkAmount)
-                            .keyboardType(.decimalPad)
-                        
-                        Picker("Number of People", selection: $numberOfPeople) {
-                            ForEach(2 ..< 100) {
-                                Text("\($0) people")
-                            }
+    }
+    
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
                         }
-                    }
-                    Section(header: Text("How much tip do you want to leave?")) {
-                        Picker("Tip Percentage", selection: $tipPercentage) {
-                            ForEach(0 ..< tipPercentages.count) {
-                                    Text("\(self.tipPercentages[$0])%")
-                            }
-                        
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
-                    }
-                    Section {
-                        Text("$\(totalPerPerson, specifier: "%.2f")")
                     }
                 }
-                .navigationBarTitle("BananaSplit")
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                        
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Amount Per Person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                Section(header: Text("Total Bill")) {
+                    Text("$\(totalBill, specifier: "%.2f")")
+                    
+                }
+               
             }
+            .navigationBarTitle("BananaSplit")
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
         }
-
+        
     }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
